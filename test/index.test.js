9b13a04e3c26785a40365c58357e7213248d4558
alemanjosh7 <josuealeman2003@gmail.com>
2024-05-17 17:53:44 -0600

    Add: brittle tests

--- test/index.test.js
+++ test/index.test.js
@@ -2,13 +2,13 @@ const test = require('brittle')
 const { init_dictionary, beekey, ask } = require('../index.js')
 
 test('init', async function (t) {
-  const response = await new Promise((resolve) => setTimeout(async () => resolve(await init_dictionary('xd')), 1000))
+  const response = await new Promise((resolve) => setTimeout(async () => resolve(await init_dictionary('test')), 1000))
   t.comment(response)
   t.pass()
 })
 
 test('ask', async function (t) {
-  const response = await new Promise((resolve) => setTimeout(async () => resolve(await ask('hypercore', 'xd')), 1000))
+  const response = await new Promise((resolve) => setTimeout(async () => resolve(await ask('hypercore', 'test')), 1000))
   t.comment(response)
   t.pass()
 })
