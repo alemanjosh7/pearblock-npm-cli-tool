fa695de3c7f657cc2b5a3c0ac1390280993ea210
alemanjosh7 <josuealeman2003@gmail.com>
2024-05-17 10:16:46 -0600

    Update index.js functions

--- index.js
+++ index.js
@@ -1,188 +1,146 @@
 #!/usr/bin/env bare
 const fsp = require('bare-fs/promises.js')
-// const Hyperswarm = require('hyperswarm')
-// const Corestore = require('corestore')
-// const Hyperbee = require('hyperbee')
-// const b4a = require('b4a')
+const Hyperswarm = require('hyperswarm')
+const Corestore = require('corestore')
+const Hyperbee = require('hyperbee')
+const b4a = require('b4a')
 const process = require('bare-process')
-// import fsp from 'bare-fs/promises.js'
-// import Hyperswarm from 'hyperswarm'
-// import Corestore from 'corestore'
-// import Hyperbee from 'hyperbee'
-// import b4a from 'b4a'
-// import process from 'bare-process'
 
-// export const cmd = readline.createInterface({
-//   input: new tty.ReadStream(0),
-//   output: new tty.WriteStream(1)
-// })
+const notFound = (command) => {
+    return `error: unknown command '${command}'`
+}
 
-// export const executeCmd = cmd.input.setMode(tty.constants.MODE_RAW)
+const missingArgument = (...args) => {
+    return `error: missing argument '${args}'`
+}
 
+const beekey = async (corekey) => {
+    const store = new Corestore('./reader-storage')
+    const swarm = new Hyperswarm()
 
+    swarm.on('connection', conn => store.replicate(conn))
 
-// console.log(process.argv[2])
+    const core = store.get({ name: corekey })
 
-// export const notFound = (command) => {
-//   return `\nerror: unknown command '${command}'`
-// }
+    const bee = new Hyperbee(core, {
+        keyEncoding: 'utf-8',
+        valueEncoding: 'utf-8'
+    })
 
-// const missingArgument = (...args) => {
-//   return `\nerror: missing argument '${args}'`
-// }
-
-// const cmdOpt = (line) => {
-//   const k = (line.length < 3 || line[2] == '')
-//   const w = (line.length < 4 || line[3] == '')
-//   const err = []
-//   if (k) err.push('key')
-//   switch (line[1]) {
-//     case 'init':
-//       if (k) {
-//         console.log(missingArgument(err))
-//       } else {
-//         init(line[2])
-//       }
-//       break
-//     case 'ask':
-//       if (w) err.push('word')
-//       if (w || k) missingArgument(err)
-//       else ask(line[2], line[3])
-//       break
-//     case 'beekey':
-//       if (k) missingArgument(err)
-//       else beekey(line[2])
-//       break
-//     default:
-//       notFound()
-//   }
-// }
-
-// export const help = cmd.on('data', lines => {
-//   if (lines.length < 10 || lines.substring(0, 10) !== 'pearblocks') {
-//     notFound()
-//   } else {
-//     const line = lines.split(' ')
-//     if (line.length > 1) {
-//       if (line[1].substring(0, 2) == '--') {
-//         if (line[1].includes('help')) {
-//           console.log(`
-// Usage: pearblocks [command] [options]\n
-// This app is a CLI dictionary that works as serverless application using hyperswarm, corestore, hyperbee, b4a, bare-readline and bare-tty, and where you can look for specific information about a Holepunch's blocks or terminology such as Hyperbee, Hyperswarm, etc\n
-// Commands:\n
-//   init <key>               Starts importing or seeding Pearblocks Dictionary with a specific Hypercore Key\n
-//   beekey <key>             With a Hypercore key you can ask what is your Hyperbee Key\n
-//   ask <word> <key>         Ask about a specific word in the dictionary with a specific Hypercore key\n`)
-//         } else {
-//           console.log(notFound())
-//         }
-//       } else {
-//         console.log(cmdOpt(line))
-//       }
-//     } else notFound()
-//   }
-//   cmd.prompt()
-// })
-// cmd.prompt()
-// export const beekey = async (corekey) => {
-//   const store = new Corestore('./reader-storage')
-//   const swarm = new Hyperswarm()
-
-//   swarm.on('connection', conn => store.replicate(conn))
-
-//   const core = store.get({ name: corekey })
-
-//   const bee = new Hyperbee(core, {
-//     keyEncoding: 'utf-8',
-//     valueEncoding: 'utf-8'
-//   })
-
-//   await core.ready()
-
-//   const discovery = swarm.join(core.discoveryKey)
-//   let answer = ''
-//   discovery.flushed().then(() => {
-//     answer = 'You can connect from another command like using this Hyperbee key:', b4a.toString(core.key, 'hex') + '\n'
-//   })
-
-//   await core.close()
-//   await store.close()
-//   console.log(answer)
-//   return answer
-// }
+    await core.ready()
+    await core.close()
+    await store.close()
+    swarm.destroy()
+    return 'You can connect from another command like using this Hyperbee key:', b4a.toString(core.key, 'hex')
+}
 
 const init_dictionary = async (corekey) => {
-  const store = new Corestore('./reader-storage')
-  const swarm = new Hyperswarm()
-  swarm.on('connection', conn => store.replicate(conn))
+    const store = new Corestore('./reader-storage')
+    const swarm = new Hyperswarm()
+    swarm.on('connection', conn => store.replicate(conn))
 
-  const core = store.get({ name: corekey })
+    const core = store.get({ name: corekey })
 
-  const bee = new Hyperbee(core, {
-    keyEncoding: 'utf-8',
-    valueEncoding: 'utf-8'
-  })
-
-  await core.ready()
-
-  const key = b4a.toString(core.key, 'hex')
-  if (core.length <= 1) {
-    console.log('Importing dictionary...\n')
-    const dict = JSON.parse(await fsp.readFile('./dict.json'))
-    const batch = bee.batch()
-    for (const { key, value } of dict) {
-      await batch.put(key, value)
+    const bee = new Hyperbee(core, {
+        keyEncoding: 'utf-8',
+        valueEncoding: 'utf-8'
+    })
+    await core.ready()
+    if (core.length <= 1) {
+        const dict = JSON.parse(await fsp.readFile('./dict.json'))
+        const batch = bee.batch()
+        for (const { key, value } of dict) {
+            await batch.put(key, value)
+        }
+        await batch.flush()
+        await core.close()
+        await store.close()
+        await swarm.destroy()
+        return 'Importing dictionary...\nDictionary imported!'
     }
-    console.log('Dictionary imported!\n')
-    await batch.flush()
-  } else {
-    console.log('Seeding dictionary...\n')
-    console.log('Dictionary seeded!\n')
-  }
-  await core.close()
-  await store.close()
+
+    await core.close()
+    await store.close()
+    await swarm.destroy()
+    return 'Seeding dictionary...\nDictionary seeded!'
+
+
 }
-// export const ask = async (word, corekey) => {
-//   const store = new Corestore('./reader-storage')
-//   const swarm = new Hyperswarm()
-//   swarm.on('connection', conn => store.replicate(conn))
-//   console.log(`The pearblock you want to know about is: ${word}`)
-//   const core = store.get({ name: corekey })
 
-//   const bee = new Hyperbee(core, {
-//     keyEncoding: 'utf-8',
-//     valueEncoding: 'utf-8'
-//   })
+const ask = async (word, corekey) => {
+    const store = new Corestore('./reader-storage')
+    const swarm = new Hyperswarm()
+    swarm.on('connection', conn => store.replicate(conn))
+    console.log(`The pearblock you want to know about is: ${word}`)
+    const core = store.get({ name: corekey })
 
-//   await core.ready()
+    const bee = new Hyperbee(core, {
+        keyEncoding: 'utf-8',
+        valueEncoding: 'utf-8'
+    })
 
-//   const key = b4a.toString(core.key, 'hex')
+    await core.ready()
 
-//   swarm.join(core.discoveryKey)
+    const key = b4a.toString(core.key, 'hex')
 
-//   if (!word.length) return
-//   let response = ''
+    swarm.join(core.discoveryKey)
 
-//   await bee.get(word).then(node => {
-//     if (!node || !node.value) {
-//       response = `No dictionary entry for ${word}\n`
-//       return response
-//     }
-//     response = `Holepunch's Block ${word} is: ${node.value}\n`
-//   })
-//   console.log(response)
-//   await core.close()
-//   await store.close()
-//   return response
-// }
-// module.exports = init_dictionary
+    if (!word.length) return
+    let response = ''
 
-async function getData() {
-    if (process.argv[2] == 'init') {
-        const data = 'lol'
-        // console.log(data)
-        console.log(await init('xd'))
+    await bee.get(word).then(node => {
+        if (!node || !node.value) {
+            response = `No dictionary entry for ${word}`
+            return response
+        }
+        response = `Holepunch's Block ${word} is: ${node.value}`
+    })
+    await core.close()
+    await store.close()
+    swarm.destroy()
+    return response
+}
+
+(async () => {
+
+    switch (process.argv[2]) {
+        case 'init':
+            if (process.argv[3] === undefined) {
+                return new Promise(async () => console.log(missingArgument('key'))).finally(setTimeout(() => { process.exit(1) }, 1000))
+            }
+            return new Promise(async () => console.log(await init_dictionary(process.argv[3]))).finally(setTimeout(() => {
+                process.exit(1)
+            }, 1000))
+        case 'beekey':
+            if (process.argv[3] === undefined) {
+                return new Promise(async () => console.log(missingArgument('key'))).finally(setTimeout(() => { process.exit(1) }, 1000))
+            }
+            return new Promise(async () => console.log(await beekey(process.argv[3]))).finally(setTimeout(() => {
+                process.exit(1)
+            }, 1000))
+        case 'ask':
+            if (process.argv[3] === undefined) {
+                return new Promise(async () => console.log(missingArgument('key'))).finally(setTimeout(() => { process.exit(1) }, 1000))
+            }
+            if (process.argv[4] === undefined) {
+                return new Promise(async () => console.log(missingArgument('word'))).finally(setTimeout(() => { process.exit(1) }, 1000))
+            }
+            return new Promise(async () => console.log(await ask(process.argv[3], process.argv[4]))).finally(setTimeout(() => {
+                process.exit(1)
+            }, 1000))
+        case 'help':
+            console.log(`
+Usage: pearblocks [command] [options]\n
+This app is a CLI dictionary that works as serverless application using hyperswarm, corestore, hyperbee, b4a, bare-readline and bare-tty, and where you can look for specific information about a Holepunch's blocks or terminology such as Hyperbee, Hyperswarm, etc\n
+Commands:\n
+  init <key>               Starts importing or seeding Pearblocks Dictionary with a specific Hypercore Key\n
+  beekey <key>             With a Hypercore key you can ask what is your Hyperbee Key\n
+  ask <key> <word>         Ask about a specific word in the dictionary with a specific Hypercore key\n`)
+            break
+        default:
+            console.log(notFound((process.argv[2])))
+            break
     }
-}
-getData()
-module.exports = getData
+
+})()
+
